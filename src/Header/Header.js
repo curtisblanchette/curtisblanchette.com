cac3b8ac262be6300cb06e4529166645edf4ebfb
Curt Blanchette <6607883+curtisblanchette@users.noreply.github.com>
2023-03-09 10:18:34 -0800

    fix(Header): add contact icons

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Heading = styled.h1`
   font-size: 9vw;
   font-weight: 900;
-  margin-bottom: 0;
+  margin-bottom: 10px;
 	color: ${ props => props.theme.color };
   line-height: .9em;
   margin-top: 1.5em;
@@ -23,18 +23,24 @@ const Heading = styled.h1`
 
 const Subheading = styled.div`
   font-weight: 300;
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: column;
+	gap: .5em;
+	
+	a {
+		text-decoration: none;
+	}
 `;
 
 const Header = () => {
 	return (
 		<>
 			<Heading>Curtis Blanchette</Heading>
-			<Subheading className="subheading">
-				<a href="mailto: hello@curtisblanchette">hello@curtisblanchette.com</a> | <a
-				href="https://linkedin.com/in/curtisblanchette" rel="noreferrer"
-				target="_blank">linkedin.com/in/curtisblanchette</a> | <a href="https://github.com/curtisblanchette/"
-				                                                          rel="noreferrer"
-				                                                          target="_blank">github.com/curtisblanchette</a>
+			<Subheading>
+				<a href="mailto: hello@curtisblanchette"><i className="fa fa-envelope"/> hello@curtisblanchette.com</a>
+				<a href="https://linkedin.com/in/curtisblanchette" rel="noreferrer" target="_blank"><i className="fa fa-linkedin"/> linkedin.com/in/curtisblanchette</a>
+				<a href="https://github.com/curtisblanchette/" rel="noreferrer" target="_blank">< i className="fa fa-github"/> github.com/curtisblanchette</a>
 			</Subheading>
 		</>
 	)
